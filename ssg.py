ee27ef2debcbbd1b135d25dd6a1ce451d611748e
sjanibekova <sdzhanybekova@seikolabs.kz>
2021-06-27 21:21:29 +0600

    asd

--- ssg.py
+++ ssg.py
@@ -1,9 +1,20 @@
-import ssg.parsers
 import typer
 from ssg.site import Site
 
+import ssg.parsers
+
+
 def main(source="content", dest="dist"):
-    config = {"source": source, "dest": dest, "parsers": [ssg.parsers.ResourceParser()] }
+    config = {
+        "source": source,
+        "dest": dest,
+        "parsers": [
+            ssg.parsers.ResourceParser(),
+            ssg.parsers.MarkdownParser(),
+            ssg.parsers.ReStructuredTextParser(),
+        ],
+    }
     Site(**config).build()
+
 
 typer.run(main)
--- ssg/parsers.py
+++ ssg/parsers.py
@@ -1,7 +1,10 @@
 from typing import List
 from pathlib import Path
 import shutil
-
+import sys
+from docutils.core import publish_parts
+from markdown import markdown
+from content import Content
 
 class Parser:
     extensions: List[str] = []
@@ -33,3 +36,25 @@ class ResourceParser(Parser):
 
     def copy(self, path, source, dest):
         shutil.copy2(path, dest / path.relative_to(source))
+class MarkdownParser(Parser):
+    extensions = [".md", ".markdown"]
+
+    def parse(self, path, source, dest):
+        content = Content.load(self.read(path))
+        html = markdown(content.body)
+        self.write(path, dest, html)
+        sys.stdout.write(
+            "\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content)
+        )
+
+
+class ReStructuredTextParser(Parser):
+    extensions = [".rst"]
+
+    def parse(self, path, source, dest):
+        content = Content.load(self.read(path))
+        html = publish_parts(content.body, writer_name="html5")
+        self.write(path, dest, html["html_body"])
+        sys.stdout.write(
+            "\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content)
+        )
